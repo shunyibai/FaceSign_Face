09a78a0825dbc4a0cb7afe5ff9a9e8a201a2f476
Shunyi Bai <baishunyi819@163.com>
2018-03-14 11:37:50 +0800

    合并2

--- app/src/main/java/humanface/pwc/com/facesign_face/util/FaceHttp.java
+++ app/src/main/java/humanface/pwc/com/facesign_face/util/FaceHttp.java
@@ -206,11 +206,13 @@ public class FaceHttp {
      * @param xCreateUserTokenCallBack
      */
     public static void recognizeBaiduText(String access_token,String base64Img, Callback.CommonCallback<String> xCreateUserTokenCallBack){
-        String path = "https://aip.baidubce.com/rest/2.0/ocr/v1/general?access_token=";
-        path = path+access_token;
-        RequestParams params = new RequestParams(path);
-        params.addHeader("Content-Type","application/x-www-form-urlencoded");
-        params.addBodyParameter("image", base64Img);
+
+
+        String path = "https://aip.baidubce.com/rest/2.0/ocr/v1/general?access_token=";//备注
+        path = path+access_token;//备注
+        RequestParams params = new RequestParams(path);//备注
+        params.addHeader("Content-Type","application/x-www-form-urlencoded");//备注
+        params.addBodyParameter("image", base64Img);//备注
         x.http().post(params, xCreateUserTokenCallBack);
         //重新提交一遍
     }
